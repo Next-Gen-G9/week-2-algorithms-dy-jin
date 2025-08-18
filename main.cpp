1fe1abfe75890dacbb7448f9e2cd8db384d79c1a
Sujin-seng <srysochensmart168@gmail.com>
2025-08-18 12:04:42 +0700

    commit

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "models/Book.h"
 #include "models/User.h"
 #include "utils/LibraryUtils.h"
+using namespace std;
 
 // --- Forward Declarations for Menu Functions ---
 void runAdminSession();
@@ -23,7 +24,7 @@ void showPromotionalBooks() {
 int main() {
     // This part handles login and routing to the correct session.
     // It is provided as a framework for your functions.
-    std::vector<User> users = loadUsers();
+    vector<User> users = loadUsers();
     while (true) {
         // ... Login menu and logic ...
         // [The full menu logic from the previous complete answer would go here]
@@ -40,16 +41,108 @@ int main() {
 void runAdminSession() {
     // The main loop for the admin is provided.
     // It calls the functions you will implement in LibraryUtils.cpp.
-    std::vector<Book> libraryBooks = loadBooks();
+    vector<Book> libraryBooks = loadBooks();
     bool booksAreSorted = false;
+    
     while(true) {
         // ... Admin menu and switch statement ...
         // Example case:
         // case 1: addBook(libraryBooks); booksAreSorted = false; break;
         // case 7: showPromotionalBooks(); break; // <-- Your new array function
+       cout << "\n ---- Admin Menu ----\n";
+        cout << "1. Add Book\n";
+        cout << "2. Remove Book\n";
+        cout << "3. View All Books\n";
+        cout << "4. Search Book by ID\n";
+        cout << "5. Sort Books by Title\n";
+        cout << "6. Sort Books by Author\n";
+        cout << "7. Show Promotional Books\n";
+        cout << "0. Logout\n";
+
+        int choice;
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                addBook(libraryBooks);
+                booksAreSorted = false;
+                break;
+            case 2:
+                removeBook(libraryBooks);
+                break;
+            case 3:
+                viewAllBooks(libraryBooks);
+                break;
+            case 4:
+                searchBookById(libraryBooks);
+                break;
+            case 5:
+                sortBooksByTitle(libraryBooks);
+                break;
+            case 6:
+                sortBooksByAuthor(libraryBooks);
+                break;
+            case 7:
+                showPromotionalBooks();
+                break;
+            case 0:
+                cout << "Logging out from this session...\n";
+                return;
+            default:
+                cout << "Invalid choice.\n";
+        }
     }
 }
 
-void runUserSession() { /* ... Provided ... */ }
-// [The full code for main, runAdminSession, etc. would be provided to the student]
-// [as it acts as the test harness for their implemented functions.]
+// --- USER SESSION ---
+void runUserSession() {
+    while (true) {
+        cout << "\n===== User Menu =====\n";
+        cout << "1. View All Books\n";
+        cout << "2. Search Book by ID\n";
+        cout << "3. Borrow Book\n";
+        cout << "4. Return Book\n";
+        cout << "5. Show Promotional Books\n";
+        cout << "0. Logout\n";
+        cout << "Enter choice: ";
+
+        int choice;
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                cout << "\n--- All Books ---\n";
+                showAllBooks();
+                break;
+            case 2: {
+                cout << "\nEnter Book ID: ";
+                int id;
+                cin >> id;
+                searchBookById(id);
+                break;
+            }
+            case 3: {
+                cout << "\nEnter Book ID to Borrow: ";
+                int id;
+                cin >> id;
+                borrowBook(id);
+                break;
+            }
+            case 4: {
+                cout << "\nEnter Book ID to Return: ";
+                int id;
+                cin >> id;
+                returnBook(id);
+                break;
+            }
+            case 5:
+                showPromotionalBooks();
+                break;
+            case 0:
+                cout << "Logging out...\n";
+                return;
+            default:
+                cout << "Invalid choice. Please try again.\n";
+        }
+    }
+}
